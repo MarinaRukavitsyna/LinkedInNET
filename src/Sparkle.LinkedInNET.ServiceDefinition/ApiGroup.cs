24427b69ec62f284983fbec30330387c9b12c842
Marina Rukavitsyna <MarinaRukavitsyna@users.noreply.github.com>
2021-02-15 14:30:38 +0100

    Update ApiGroup.cs

--- src/Sparkle.LinkedInNET.ServiceDefinition/ApiGroup.cs
+++ src/Sparkle.LinkedInNET.ServiceDefinition/ApiGroup.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace Sparkle.LinkedInNET.ServiceDefinition
 {
     using System;
@@ -8,20 +8,82 @@ namespace Sparkle.LinkedInNET.ServiceDefinition
     using System.Xml.Serialization;
 
     [Serializable]
-    public class ApiGroup
+    public class ApiMethod
     {
-        [XmlAttribute(AttributeName="Name")]
-        public string Name { get; set; }
+        private bool useFieldSelectors = true;
 
-        [XmlElement(ElementName = "ApiMethod")]
-        public List<ApiMethod> Methods { get; set; }
+        [XmlAttribute]
+        public string Title { get; set; }
 
-        [XmlElement(ElementName = "ReturnType")]
-        public List<ReturnType> ReturnTypes { get; set; }
+        [XmlAttribute]
+        public string Remark { get; set; }
+
+        [XmlAttribute]
+        public string Path { get; set; }
+
+        /// <summary>
+        /// use this filed to skip escaping path urlparams
+        /// list lit comma separated values
+        /// e.g.: urn,id,postId
+        /// </summary>
+        [XmlAttribute]
+        public string SkipUrlParamsEscape { get; set; }
+
+        /// <summary>
+        /// people, ugc apis work with this protocol
+        /// </summary>
+        [XmlAttribute]
+        public bool UseRestliProtocol { get; set; }
+
+        [XmlAttribute]
+        public string ReturnType { get; set; }
+
+        [XmlAttribute]
+        public string PostReturnType { get; set; }
+
+        [XmlAttribute]
+        public string MethodName { get; set; }
+
+        [XmlAttribute]
+        public string HttpMethod { get; set; }
+
+        [XmlAttribute]
+        public string Query { get; set; }
+
+        [XmlAttribute]
+        public bool RequiresTunneling { get; set; }
+
+        [XmlAttribute]
+        public bool RequiresUserAuthentication { get; set; }
+
+        [XmlAttribute]
+        public bool UsesAcceptLanguage { get; set; }
+
+        [XmlAttribute]
+        public bool UseFieldSelectors
+        {
+            get { return this.useFieldSelectors; }
+            set { this.useFieldSelectors = value; }
+        }
+
+        [XmlAttribute]
+        public bool IsValueType { get; set; }
+
+        [XmlAttribute]
+        public bool IsStreamOctetResultHandeling { get; set; }
+
+        [XmlAttribute]
+        public bool IsIntResultHandeling { get; set; }
+
+        [XmlAttribute]
+        public bool IsStringResultHandeling { get; set; }
+
+        [XmlAttribute]
+        public bool IsUGCPostHandeling { get; set; }
 
         public override string ToString()
         {
-            return "ApiGroup(N='" + Name + "', M=" + (Methods != null ? Methods.Count : 0) + ", RT=" + (ReturnTypes != null ? ReturnTypes.Count : 0) + ")";
+            return string.Format("ApiMethod({0}{1}{2}{3})", HttpMethod, Path, MethodName, Title);
         }
     }
 }
